69e32cb383d62b4b4366bd8f364b6e30375351e3
Miguel Cortez <mickeycortez2002@gmail.com>
2024-12-10 22:10:35 -0600

    Cambios para movil y web en relacion de aspecto de imagenes / comentarios_2

--- App.js
+++ App.js
@@ -4,8 +4,17 @@ useEffect: un hook de react que se utiliza para manejar efectos secundarios (com
 useState: Una variable que cada que cambia de valor se vuelven a renderizar los componentes
 un hook de react que permite agregar y manejar estado dentro de un componente funcional
 */
+import {
+  Text,
+  View,
+  Image,
+  ScrollView,
+  StyleSheet,
+  StatusBar,
+  SafeAreaView,
+  Platform,
+} from "react-native";
 
-import {Text,View,Image,ScrollView,StyleSheet,StatusBar,SafeAreaView,} from "react-native"; 
 /*
 Text: Componente para mostrar el texto en la interfaz
 View: Componente contenedor que agrupa otros componentes y permite aplicar estilos
@@ -14,16 +23,18 @@ ScrollView: Componente para hacer scroll en vistas con contenido que excede el t
 StyleSheet: API para definir estilos en react native (Esta hasta la parte de los estilos)
 StatusBar: Componente para personalizar la barra de estado (color, estilo, etc) (esta es donde sale la hora, bateria etc)
 SafeAreaView: Componente que asegura que el contenido no se superponga con las areas seguras del dispositivo (statusbar o notch)
+Platform: Para checar las plataformas movil, web
 */
-import { getLatestGames, getGameDetails } from "./lib/rawg.js"; //Importamos funciones del archivo con la API
+import { getRatingGames, getGameDetails } from "./lib/rawg.js"; //Importamos funciones del archivo con la API
+
 
 export default function App() {
-  const [games, setGames] = useState([]);
+  const [games, setGames] = useState([]); //games es una lista que almacen los juegos de la API
   const [selectedGame, setSelectedGame] = useState(null);
 
   useEffect(() => {
     async function fetchGames() {
-      const latestGames = await getLatestGames();
+      const latestGames = await getRatingGames();
       setGames(latestGames);
     }
     fetchGames();
@@ -38,10 +49,10 @@ export default function App() {
     <SafeAreaView style={styles.container}>
       <StatusBar style="light" />
       <ScrollView contentContainerStyle={styles.content}>
-        <Text style={styles.header}>Latest Games</Text>
+        <Text style={styles.header}>Popular Games</Text>
         {games.map((game) => (
           <View key={game.id} style={styles.card}>
-            <Image source={{ uri: game.image }} style={styles.image} />
+            <Image source={{ uri: game.image }} style={styles.imageCard} />
             <Text style={styles.title}>{game.title}</Text>
             <Text style={styles.rating}>Rating: {game.rating}</Text>
             <Text
@@ -55,7 +66,14 @@ export default function App() {
         {selectedGame && (
           <View style={styles.details}>
             <Text style={styles.header}>{selectedGame.title}</Text>
-            <Image source={{ uri: selectedGame.image }} style={styles.image} />
+            <Image
+              source={{ uri: selectedGame.image }}
+              style={styles.image}
+              onError={(e) =>
+                console.error("Error cargando la imagen:", e.nativeEvent.error)
+              }
+            />
+            <Text>Rating: {selectedGame.rating}</Text>
             <Text>{selectedGame.description}</Text>
             <Text>Genres: {selectedGame.genres.join(", ")}</Text>
             <Text>Platforms: {selectedGame.platforms.join(", ")}</Text>
@@ -82,20 +100,48 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     textAlign: "center",
   },
+  subHeader: {
+    fontSize: 20,
+    fontWeight: "600",
+    marginTop: 20,
+    textAlign: "center",
+  },
   card: {
     marginBottom: 20,
     borderWidth: 1,
     borderColor: "#ddd",
     padding: 10,
-    width: "90%",
+    width: "80%",
     alignItems: "center",
     borderRadius: 8,
   },
-  image: {
-    width: "100%",
-    height: 200,
-    borderRadius: 10,
-  },
+  imageCard: Platform.select({
+    web: {
+      width: "50%", // Ocupa el 50% del ancho del contenedor
+      aspectRatio: 16 / 9, // Mantén la relación de aspecto 16:9
+      borderRadius: 10,
+    },
+    default: {
+      width: "100%", // En móviles, ocupa todo el ancho del contenedor
+      aspectRatio: 16 / 9, // Mantén la relación de aspecto
+      borderRadius: 10,
+    },
+  }),
+  image: Platform.select({
+    web: {
+      width: "40%", // En web, ocupa el 40% del ancho del contenedor
+      height: undefined,
+      aspectRatio: 16 / 9, // Mantén la relación de aspecto
+      borderRadius: 10,
+    },
+    default: {
+      width: "80%", // En móviles, ocupa todo el ancho del contenedor
+      aspectRatio: 16 / 9, // Mantén la relación de aspecto
+      height: undefined, // Al no poner el `height`, la imagen toma el tamaño adecuado por el `aspectRatio`
+      borderRadius: 10,
+    },
+  }),
+  
   title: {
     fontSize: 18,
     fontWeight: "bold",
