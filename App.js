43b7423ebd369206eaba3d0f41f0b1502347ae2a
Miguel Cortez <mickeycortez2002@gmail.com>
2024-12-10 19:58:56 -0600

    Comentarios_1

--- App.js
+++ App.js
@@ -1,6 +1,21 @@
 import React, { useEffect, useState } from "react";
-import {Text,View,Image,ScrollView,StyleSheet,StatusBar,SafeAreaView,} from "react-native";
-import { getLatestGames, getGameDetails } from "./lib/metacritic.js";
+/*
+useEffect: un hook de react que se utiliza para manejar efectos secundarios (como llamadas a APIs o suscripciones)
+useState: Una variable que cada que cambia de valor se vuelven a renderizar los componentes
+un hook de react que permite agregar y manejar estado dentro de un componente funcional
+*/
+
+import {Text,View,Image,ScrollView,StyleSheet,StatusBar,SafeAreaView,} from "react-native"; 
+/*
+Text: Componente para mostrar el texto en la interfaz
+View: Componente contenedor que agrupa otros componentes y permite aplicar estilos
+Image: Componente para mostrar imagenes
+ScrollView: Componente para hacer scroll en vistas con contenido que excede el tamaño de la pantalla
+StyleSheet: API para definir estilos en react native (Esta hasta la parte de los estilos)
+StatusBar: Componente para personalizar la barra de estado (color, estilo, etc) (esta es donde sale la hora, bateria etc)
+SafeAreaView: Componente que asegura que el contenido no se superponga con las areas seguras del dispositivo (statusbar o notch)
+*/
+import { getLatestGames, getGameDetails } from "./lib/rawg.js"; //Importamos funciones del archivo con la API
 
 export default function App() {
   const [games, setGames] = useState([]);
