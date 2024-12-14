3d44fd7674662e4c72e256796ab679eed0051e47
Miguel Cortez <mickeycortez2002@gmail.com>
2024-12-14 15:22:23 -0600

    Logo

--- components/main.jsx
+++ components/main.jsx
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { FlatList, Text, View, StyleSheet, ActivityIndicator } from "react-native";
+import {
+  FlatList,
+  Text,
+  View,
+  StyleSheet,
+  ActivityIndicator,
+} from "react-native";
 import { getRatingGames } from "../lib/rawg.js"; // Importamos la API para obtener los juegos
 import { GameCard } from "./gameCard.jsx"; // Componente que muestra los juegos
 import { useSafeAreaInsets } from "react-native-safe-area-context"; // Para manejar los márgenes seguros en dispositivos móviles
+import { Logo } from "./logo.jsx";
 
 export function Main() {
   const [games, setGames] = useState([]); // Lista de juegos
@@ -20,7 +27,21 @@ export function Main() {
   }, []);
 
   return (
-    <View style={[styles.container, { paddingTop: insets.top, paddingBottom: insets.bottom }]}>
+    <View
+      style={[
+        styles.container,
+        { paddingTop: insets.top, paddingBottom: insets.bottom },
+      ]}
+    >
+      <View style={styles.header_Principal}>
+        <Logo
+          fill="#fff"
+          width={80}
+          height={70}
+          style={{marginLeft: 10}}
+        />
+      </View>
+
       <Text style={styles.header}>Popular Games</Text>
       {/* Mostrar ActivityIndicator mientras cargan los datos */}
       {/* "SI SE ESTAN CARGANDO" se muestra el indicador de carga, "NO SE ESTAN CARGANDO" osea ya termino la carga muestra los juegos
@@ -57,5 +78,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  header_Principal: {
+   // backgroundColor: "red",
+    width: "100%",
+    height: 90,
+    paddingTop: 10, // Espaciado superior
+    paddingBottom: 10, // Espaciado inferior
+    marginBottom: 10,
   },
 });
